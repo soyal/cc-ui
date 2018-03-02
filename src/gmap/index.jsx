ea7d0bbaf6e70b22028812080c89ce1ace6c897d
Soyal <345723940@qq.com>
2018-03-02 14:31:53 +0800

    V2.2.1 (#3)
    * v2.2.1:
    fix:修复Gmap死循环进行setCoordinate的问题

--- src/gmap/index.jsx
+++ src/gmap/index.jsx
@@ -11,34 +11,35 @@ let AMap = window.AMap
 
 function loadAMap(cb) {
   const script = document.createElement('script')
-  script.src = '//webapi.amap.com/maps?v=1.3&key=26339b2f15f1daeefb8eacb7130bc0c3'
+  script.src =
+    '//webapi.amap.com/maps?v=1.3&key=26339b2f15f1daeefb8eacb7130bc0c3'
   script.async = false
   document.head.appendChild(script)
   script.onload = cb
 }
-
 
 /**
  * 在点击了联想词或者点击了搜索之后调用回调传递经纬度
  */
 class FsMap extends Component {
   static propTypes = {
-    coordinate: PropTypes.array,  // 地图初始化的时候的经纬度
+    coordinate: PropTypes.array, // 地图初始化的时候的经纬度 设置为[0, 0]componentDidMount后不会触发onChange，设置null会回到天府新区的地理位置
     /**
      * 每次用户通过搜索或者点击操作更改了经纬度的时候，触发，会返回一个对象
      * {coordinate: [lng, lat], success: 是否获取地址成功, address: 逆向编码获取到的地址}
      */
     onChange: PropTypes.func,
-    noty: PropTypes.object  // 实现了noty接口的提示器
+    noty: PropTypes.object // 实现了noty接口的提示器
   }
 
   static defaultProps = {
-    coordinate: [0, 0],
+    //104.065751,30.657571 为天府广场的经纬度
+    coordinate: [104.065751, 30.657571],
     noty: {
-      success: () => { },
-      info: () => { },
-      warning: () => { },
-      error: () => { }
+      success: () => {},
+      info: () => {},
+      warning: () => {},
+      error: () => {}
     }
   }
 
@@ -46,8 +47,7 @@ class FsMap extends Component {
     super(props)
 
     this.state = {
-      keyValue: '',  //搜索的关键字
-      coordinate: props.coordinate
+      keyValue: '' //搜索的关键字
     }
   }
 
@@ -68,11 +68,10 @@ class FsMap extends Component {
 
       // 点击联想词后的回调
       let map = this.map
-      AMap.event.addListener(auto, 'select', (e) => {
+      AMap.event.addListener(auto, 'select', e => {
         map.setZoom(15)
         // 标记
         this.setCoordinate(e.poi.location)
-
       })
     })
   }
@@ -96,22 +95,21 @@ class FsMap extends Component {
   loadGeolocation() {
     this._loadPlugin('AMap.Geolocation', () => {
       let geolocation = new AMap.Geolocation({
-        enableHighAccuracy: true,//是否使用高精度定位，默认:true
-        timeout: 5000,          //超过10秒后停止定位，默认：无穷大
-        maximumAge: 0,           //定位结果缓存0毫秒，默认：0
-        convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
-        showButton: true,        //显示定位按钮，默认：true
-        buttonPosition: 'RB',    //定位按钮停靠位置，默认：'LB'，左下角
-        buttonOffset: new AMap.Pixel(22, 23),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-        showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
-        showCircle: false,        //定位成功后用圆圈表示定位精度范围，默认：true
-        panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
-        zoomToAccuracy: false      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-      });
+        enableHighAccuracy: true, //是否使用高精度定位，默认:true
+        timeout: 5000, //超过10秒后停止定位，默认：无穷大
+        maximumAge: 0, //定位结果缓存0毫秒，默认：0
+        convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+        showButton: true, //显示定位按钮，默认：true
+        buttonPosition: 'RB', //定位按钮停靠位置，默认：'LB'，左下角
+        buttonOffset: new AMap.Pixel(22, 23), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+        showMarker: true, //定位成功后在定位到的位置显示点标记，默认：true
+        showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
+        panToLocation: true, //定位成功后将定位到的位置作为地图中心点，默认：true
+        zoomToAccuracy: false //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+      })
 
       this.map.addControl(geolocation)
       this.geolocation = geolocation
-
     })
   }
 
@@ -125,18 +123,7 @@ class FsMap extends Component {
         extensions: 'base'
       })
 
-      //104.065751,30.657571 为天府广场的经纬度
-      // 自动定位天府广场, 如果当前state有值，则不设置
-      // 尽量将时间定长点，因为担心进入编辑页面的时候，拉取完远端数据，会进行两次定位
-      this.timeout = window.setTimeout(() => {
-
-        if (!this.props.coordinate
-          || (this.props.coordinate[0] === 0 && this.props.coordinate[1] === 0)) {
-          this.setCoordinate([104.065751, 30.657571], true)
-        } else {
-          this.setCoordinate(this.props.coordinate, true)
-        }
-      }, 1000)
+      this.setCoordinate(this.props.coordinate, true)
     })
   }
 
@@ -144,7 +131,7 @@ class FsMap extends Component {
    * 监听对地图的点击，记录经纬度，设置marker
    */
   listenMapClick() {
-    this.map.on('click', (e) => {
+    this.map.on('click', e => {
       let longitutde = e.lnglat.getLng()
       let latitude = e.lnglat.getLat()
 
@@ -198,8 +185,8 @@ class FsMap extends Component {
         target = [location.lng, location.lat]
       }
 
-      // 是否在等待进行远程数据拉取，防止两个值不断互相设置
-      this.ispending = true
+      // 默认[0, 0]不进行逆地理编码
+      if(target[0] === 0 && target[1] === 0) return
 
       if (!this.geocoder) {
         noty.warning('逆地理编码插件加载失败')
@@ -207,21 +194,9 @@ class FsMap extends Component {
       }
       // 进行逆地理编码，根据经纬度获取具体位置信息
       this.geocoder.getAddress(target, (status, result) => {
-        this.ispending = false
-
-        // 将数据保存到state上
-        this.setState({
-          coordinate: target
-        })
-
-        // 在pending期间的函数会被缓存，等待pending结束后再执行
-        if (this.tempSet) {
-          this.tempSet()
-          return
-        }
         // 获取地址成功
         if (status === 'complete' && result.info === 'OK') {
-          let address = result.regeocode.formattedAddress; //返回地址描述
+          let address = result.regeocode.formattedAddress //返回地址描述
 
           // 定位成功进行提示
           if (!cancelNoty) {
@@ -243,7 +218,6 @@ class FsMap extends Component {
           })
         }
       })
-
     }
   }
 
@@ -252,8 +226,9 @@ class FsMap extends Component {
    * @param {Array} locatino [longitude, latitude]
    * @param {Boolean} cancelNoty 取消提示
    * @param {Boolean} cancelMarker 取消进行marker地位，只进行视觉定位
+   * @param {Boolean} triggerOnChange 是否触发onChange
    */
-  setCoordinate(location, cancelNoty, cancelMarker) {
+  setCoordinate(location, cancelNoty, cancelMarker, triggerOnChange = true) {
     this.map.setCenter(location)
 
     if (cancelMarker) {
@@ -270,24 +245,25 @@ class FsMap extends Component {
         draggable: true
       })
 
-      AMap.event.addListener(this.marker, 'dragend', (e) => {
+      AMap.event.addListener(this.marker, 'dragend', e => {
         const lnglat = e.lnglat
         const coor = [lnglat.lng, lnglat.lat]
         this.callBackCoordinate(coor)
       })
     }
 
-    this.callBackCoordinate(location, cancelNoty)
+    if (triggerOnChange) {
+      this.callBackCoordinate(location, cancelNoty)
+    }
   }
 
   // 执行搜索，搜索结果的第一个设置为用户的经纬度
   search() {
     const noty = this.props.noty
 
-    if (this.state.keyValue === '') return false;
+    if (this.state.keyValue === '') return false
 
     this.placeSearch.search(this.state.keyValue, (status, result) => {
-
       if (status === 'complete' && result.info === 'OK') {
         let location = result.poiList.pois[0].location
         this.setCoordinate(location)
@@ -302,17 +278,19 @@ class FsMap extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.coordinate && nextProps.coordinate[0] && nextProps.coordinate[1]) {
-      if (nextProps.coordinate[0] === this.state.coordinate[0]
-        && nextProps.coordinate[1] === this.state.coordinate[1]) {
+    if (
+      nextProps.coordinate &&
+      nextProps.coordinate[0] &&
+      nextProps.coordinate[1]
+    ) {
+      if (
+        nextProps.coordinate[0] === this.props.coordinate[0] &&
+        nextProps.coordinate[1] === this.props.coordinate[1]
+      ) {
         return false
       } else {
         // 默认经纬度的逆地理编码等待中
-        if (this.ispending) {
-          this.tempSet = this.setCoordinate.bind(this, nextProps.coordinate, true)
-          return
-        }
-        this.setCoordinate(nextProps.coordinate, true)
+        this.setCoordinate(nextProps.coordinate, true, false, false)
       }
     }
   }
@@ -324,32 +302,40 @@ class FsMap extends Component {
         <div className="cc-map-toolbar">
           {/*搜索*/}
           <div className="cc-map-search">
-            <input id="searchInput" ref="searchInput" type="text" className="map-search-input"
+            <input
+              id="searchInput"
+              ref="searchInput"
+              type="text"
+              className="map-search-input"
               placeholder="查找位置"
               value={this.state.keyValue}
               onChange={this.onSearchInputChange.bind(this)}
-              onKeyUp={(e) => {
+              onKeyUp={e => {
                 if (e.keyCode === 13) {
                   // this.search()
                 }
-              }} />
+              }}
+            />
             <span className="map-search-icon" onClick={this.search.bind(this)}>
-              <i className="cc-icon cc-icon-sousuo"></i>
+              <i className="cc-icon cc-icon-sousuo" />
             </span>
           </div>
 
           {/*放大缩小*/}
           <div className="cc-map-scale">
             <span className="map-scale-zoomin" onClick={this.zoomIn.bind(this)}>
-              <i className="cc-icon cc-icon-tianjia1"></i>
+              <i className="cc-icon cc-icon-tianjia1" />
             </span>
-            <span className="map-scale-zoomout" onClick={this.zoomOut.bind(this)}>
-              <i className="cc-icon cc-icon-zhankai"></i>
+            <span
+              className="map-scale-zoomout"
+              onClick={this.zoomOut.bind(this)}
+            >
+              <i className="cc-icon cc-icon-zhankai" />
             </span>
           </div>
         </div>
         {/*地图容器*/}
-        <div className="cc-map" id="map-container" ref="mapContainer"></div>
+        <div className="cc-map" id="map-container" ref="mapContainer" />
       </div>
     )
   }
@@ -371,7 +357,7 @@ class FsMap extends Component {
     if (!AMap) {
       loadAMap(() => {
         AMap = window.AMap
-        
+
         this.init()
       })
     } else {
--- stories/demo/demo-gmap.jsx
+++ stories/demo/demo-gmap.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import AMap from 'gmap'
+import React, { Component } from 'react'
+import AMap from '../../src/gmap'
 
 import 'gmap/index.css'
 
-const Demo = () => {
-  return (
-    <div style={{
-      width: '400px',
-      height: '400px'
-    }}>
-      <AMap></AMap>
-    </div>
-  )
+class Demo extends Component {
+  state = {
+    coordinate: [0, 0]
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        coordinate: [104.065751, 30.657571]
+      })
+    }, 500)
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          width: '400px',
+          height: '400px'
+        }}
+      >
+        <AMap
+          coordinate={this.state.coordinate}
+          onChange={({ coordinate, success, address }) => {
+            console.log('gmap onchange', success, address)
+          }}
+        />
+      </div>
+    )
+  }
 }
 
 export default Demo
